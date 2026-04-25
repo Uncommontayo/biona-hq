bf713f7277e5719fc47e7aeb6439f7e6358fee3e
Temitayo Ayuba Abiona <temitayo412@gmail.com>
2026-04-25 19:18:51 +0100

    fix: remove build_msvc artifacts and fix submodule conflict

--- snie/build_msvc/tests/eval/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Home/Downloads/biona_axon/snie/biona/axon/tests/eval
-# Build directory: C:/Users/Home/Downloads/biona_axon/snie/build_msvc/tests/eval
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
